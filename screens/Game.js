0e0bd33c610af12c8ae577a1af9cdc281cf68b06
nathan <kvngnathan8420@gmail.com>
2023-06-11 06:47:07 +0000

    Add game logic

--- screens/Game.js
+++ screens/Game.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
-export default function Game(){
+//
+import { useNavigation } from "@react-navigation/native";
+
+export default function Game() {
+  const baseNumber = Math.floor(Math.random() * 100);
+  const score = Math.floor(Math.random() * 100);
+
+  const [choice, setchoice] = useState("");
+
+  //
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (choice) {
+      const winner =
+        (choice === "higher" && score > baseNumber) ||
+        (choice === "lower" && baseNumber > score);
+
+        Alert.alert(`You've ${winner ? 'won' : 'lost' }`, `You scored ${score}`)
+        navigation.goBack();
+        
+    }
+  }, [baseNumber, score , choice ]);
+
   return (
-    
     <View style={styles.container}>
-        <Text>Game</Text>
+      <Text>Starting : {baseNumber}</Text>
+      <Button onPress={() => setchoice("higher")} title="Higher" />
+      <Button onPress={() => setchoice("lower")} title="Lower" />
     </View>
-
-  )
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        backgroundColor: '#fff',
-        justifyContent : 'center',
-        alignItems : 'center'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
